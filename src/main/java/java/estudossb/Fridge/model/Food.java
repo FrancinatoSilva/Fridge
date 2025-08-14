81b1e238ca2a08a1ca977bb5796a2fe3675bd8c0
Francis <irenofrancinato@gmail.com>
2025-08-13 21:08:46 -0300

    Conclusão da classe Food

--- src/main/java/java/estudossb/Fridge/model/Food.java
+++ src/main/java/java/estudossb/Fridge/model/Food.java
@@ -1,4 +1,62 @@
 package java.estudossb.Fridge.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate expirationDate;
+    private Integer quantity;
+
+    public Food() {
+
+    }
+
+    public Food(Long id, String name, LocalDate expirationDate, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.expirationDate = expirationDate;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
